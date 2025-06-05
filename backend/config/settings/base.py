e453e488abbe59064925e6a057fccbec07fb2646
Qianqian Qin <qianqian.qin@outlook.com>
2025-06-05 19:43:02 +0300

    feat: Enhance Docker configuration

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-g#3&_0u#ckd9s*lg_a-^ql0$-zjv&(4m+rhttw2ys%+na1j&bf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
 
 # Application definition
